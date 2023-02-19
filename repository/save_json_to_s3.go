d80c4f88df0a53805a8d6013403fd31dc0e1582c
linho <linho301150@gmail.com>
2023-02-20 01:51:45 +0900

    Fix: S3에 파일 저장되는 형식 변경 & Docker ENV 적용불가 확인

--- repository/save_json_to_s3.go
+++ repository/save_json_to_s3.go
@@ -18,8 +18,8 @@ func SaveJsonToS3(content []byte) {
 	fmt.Println("Start process ...")
 	fmt.Println("Download JSON in AWS S3")
 	seoul, _ := time.LoadLocation("Asia/Seoul")
-	timeData := time.Now().In(seoul)
-	item := timeData.String() + ".json"
+	timeData := time.Now().In(seoul).Format(time.RFC3339)
+	item := timeData + ".json"
 	bucket := "italian-bmt-bucket"
 	accessKeyID := os.Getenv("ACCESSKEYID")
 	accessKeySecret := os.Getenv("ACCESSKEYSECRET")
